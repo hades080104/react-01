3a32fbbfa592d8a1b0444691f9e76690c9524fb6
DangNgocChi <146684356+hades080104@users.noreply.github.com>
2024-02-14 20:20:38 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Data from "./componens/layout/Data";
 
-function App() {
-    return <Data />;
+import "./App.css";
+import React from "react";
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { taskName: '', tasks: []};
+    }
+    
+    myTaskChangeHandler = (event) => {
+        this.setState({ taskName: event.target.value });
+        // console.log('myTaskChangeHandler', this.state.taskName);
+    };
+
+    addTask = () => {
+        // console.log('addTask', this.state.taskName);
+        this.state.tasks.push(this.state.taskName);
+        this.setState({ taskName: ''});
+    };
+
+    render() {
+        return(
+            <div className="App">
+                <header className="App-header">
+                    TODO LIST
+                    <br>
+                    </br>
+                    <div className="aligned">
+                    <img
+                        src='./assets/4115237_add_plus_icon.png'
+                        alt='Add Task' 
+                        width='50px'
+                        style={{ cursor: 'pointer'}}
+                        onClick={() => this.addTask()}
+                        title="bấm để thêm!"
+                    />
+                    <input 
+                        type="text" 
+                        value={this.state.taskName}
+                        onChange={this.myTaskChangeHandler} 
+                    />
+                </div>
+                <ul>
+                    {this.state.tasks.map((value, index) => {
+                        return <li key={index}>{value}</li>;
+                    })}
+                </ul>
+                </header> 
+            </div>
+            );
+            }
 }
 
 export default App;
